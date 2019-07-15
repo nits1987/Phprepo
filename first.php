8b73ecac4d221cc67de424225ca9a3863aced4eb
nits1987 <nitind0987@gmail.com>
2019-07-15 12:37:23 +0530

    Added Codefiles

---
+++ first.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <h3>php devops pipeline via codepipeline in AWS</h3>
+ <?php echo '<p >Sample Php Code written within Html</p>'; ?>
+ </body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <h3>Main File</h3>
+ <?php echo '<p>Php Code in Html</p>'; 
+ echo time();
+ ?>
+ </body>
+</html>
